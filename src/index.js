352d4423f6969dd1a23cc423e3831f1b913591cf
Salatiel <sosorno@marathongroup.mx>
2019-01-21 10:54:28 -0600

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.jpeg';
-import Data  from './data.xml';
+import printMe from './print.js';
 
 function component() {
     let element = document.createElement('div');
+    var btn = document.createElement('button');
 
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
 
-    var myIcon  = new Image();
-    myIcon.src = Icon;
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
 
-    element.appendChild(myIcon);
-
-    console.log(Data);
+    element.appendChild(btn);
 
     return element;
 }
