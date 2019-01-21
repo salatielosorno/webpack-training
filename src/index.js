729da4614213fed432cd89f4446ab8626a159861
Salatiel <sosorno@marathongroup.mx>
2019-01-21 16:45:41 -0600

    Tree Shaking

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import _ from 'lodash';
+import { cube }  from './math.js';
 
 function component() {
-    let element = document.createElement('div');
+    let element = document.createElement('pre');
 
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = [
+        'Hello webpack!',
+        '5 cubed is equal to ' + cube(5)
+    ].join('\n\n');
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+    optimization:{
+        usedExports: true
     }
 }
